14e7afc04e1d562716eb553f209f796595903304
Marisol Chavez Robles <marisolchavezroble12@gmail.com>
2025-05-09 19:37:56 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = " https://cnt-56f9893e-49af-449f-b139-3c8c75a3c071.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
